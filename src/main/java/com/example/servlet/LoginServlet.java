0e545965d1ff49095502ed66f07b561a7ca6a02d
Artur Semenas <uncelfuka@gmail.com>
2024-01-09 11:31:47 +0100

    Module #2. Task #2. Servlets

--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,36 @@
 package com.example.servlet;
 
-public class LoginServlet {
-    //write your code here!
+import com.example.Users;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        if (session.getAttribute("user")==null){
+            req.getRequestDispatcher("login.jsp").forward(req, resp);
+        } else req.getRequestDispatcher("/user/hello.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Users users = Users.getInstance();
+
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+
+        if(users.getUsers().contains(login) && !password.isEmpty()){
+            session.setAttribute("user", login);
+            resp.sendRedirect("/user/hello.jsp");
+        } else req.getRequestDispatcher("login.jsp").forward(req,resp);
+    }
 }
